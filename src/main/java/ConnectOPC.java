a9474c0f1071a4431354639fee0b315a6d5c7f7f
Cuheyman <cuheymankaya@live.dk>
2020-10-26 10:06:34 +0100

    Connect class and Collect class
    Chances

---
+++ src/main/java/ConnectOPC.java
@@ -0,0 +1,22 @@
+import org.apache.log4j.varia.NullAppender;
+import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import java.util.List;
+
+
+
+
+    public class ConnectOPC {
+
+            public static void main(String[] args) {
+            try {
+                org.apache.log4j.BasicConfigurator.configure(new Null Appender());
+                List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://localhost:12686/milo").get();
+                System.out.println("Endpoints - " + endpoints);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+    }
