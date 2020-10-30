a6dc5d7657c6ece3750a86819aba11e308b37a28
Schachster <anni619@student.sdu.dk>
2020-10-30 11:00:15 +0100

    Moved connect functionality away from read class
    and kept it in connector class

--- src/Domain/Read.java
+++ src/Domain/Read.java
@@ -11,6 +11,7 @@ import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Read {
     public static void main(String[] args) {
@@ -19,24 +20,12 @@ public class Read {
 
     /*skal forestille at være en metode der henter en specifik variabels værdi, metoden skal overloades, følgende
     er en liste af getter metoder til at hente disse*/
-    public static void getMachineSpeed() {
-        readValue(6, "::Program:Cube.Status.MachSpeed", "machine speed" );
-    }
-    public static void getCurrentProduct() {
-        readValue(6, "::Program:Cube.Admin.Parameter[0].Value", "current product" );
-    }
-    public static void getCurrentBatchId() {
-        readValue(6, "::Program:Cube.Status.Parameter[0].Value", "current batch id");
-    }
-    public static void getCurrentQuantity() {
-        readValue(6, "::Program:Cube.Status.Parameter[1].Value", "current quantity" );
-    }
-    public static void getProductsProduced(){
-        readValue(6, "::Program:Cube.Admin.ProdProcessedCount", "products produced");
-    }
-    public static void getFailedProductsProduced(){
-        readValue(6, "::Program:Cube.Admin.ProdDefectiveCount", "defective products produced");
-    }
+    public static void getMachineSpeed() { readValue(6, "::Program:Cube.Status.MachSpeed", "machine speed" ); }
+    public static void getCurrentProduct() { readValue(6, "::Program:Cube.Admin.Parameter[0].Value", "current product" ); }
+    public static void getCurrentBatchId() { readValue(6, "::Program:Cube.Status.Parameter[0].Value", "current batch id"); }
+    public static void getCurrentQuantity() { readValue(6, "::Program:Cube.Status.Parameter[1].Value", "current quantity" ); }
+    public static void getProductsProduced(){ readValue(6, "::Program:Cube.Admin.ProdProcessedCount", "products produced"); }
+    public static void getFailedProductsProduced(){ readValue(6, "::Program:Cube.Admin.ProdDefectiveCount", "defective products produced"); }
     public static void getStopReasonId(){
         readValue(6, "::Program:Cube.Admin.StopReason.ID", "stop reason id");
     }
@@ -49,29 +38,23 @@ public class Read {
     public static void getTemperature(){
         readValue(6,"::Program:Cube.Status.Parameter[3].Value", "temperature");
     }
-    
+
+    private static OpcUaClient client = null;
+
     public static void readValue(int namespaceindex, String identifier, String parameterName) {
         //At first we try to establish a connection to the software simulator url, if it connects we want it to print all endpoints
-        try {
-            org.apache.log4j.BasicConfigurator.configure(new NullAppender());
-            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
-
-            /*now we want to read specific nodes from the simulator
-            firstly we create a OpcUaClientConfigBuilder object */
-            OpcUaClientConfigBuilder configBuilder = new OpcUaClientConfigBuilder();
-            // we set the endpoint of our object to the first endpoint from the list
-            configBuilder.setEndpoint(endpoints.get(0));
-
-            /*now we want to create a OpcUaClient object with the configuration from the previously created
-            config object*/
-            OpcUaClient client = OpcUaClient.create(configBuilder.build());
-            client.connect().get();
+        client = OpcUAConnector.getOpcUaClient();
 
             /* to read the value from a specific node, we can provide the namespace index as
             the first parameter, and the identifier string as the second parameter for the
             specific nodeId. Here we want to read the value corresponding to the cube.command.Machspeed */
             NodeId nodeId = new NodeId(namespaceindex, identifier);
-            DataValue dataValue =client.readValue(0, TimestampsToReturn.Both, nodeId).get();
+        DataValue dataValue = null;
+        try {
+            dataValue = client.readValue(0, TimestampsToReturn.Both, nodeId).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
             /* now to simplify the response from the read operation, we want to store the read value that goes by
             "variant" as the variable specific to the operation. Here we start out by doing it for the machine speed
@@ -127,10 +110,6 @@ public class Read {
                     System.out.println("Temperature = " + temperature);
                     break;
             }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 
 
