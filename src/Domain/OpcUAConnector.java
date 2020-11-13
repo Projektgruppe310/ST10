015bfa7134f213d4c92781d3e40ccb801147b179
Schachster <anni619@student.sdu.dk>
2020-11-13 10:38:59 +0100

    Thomas patch

--- src/Domain/OpcUAConnector.java
+++ src/Domain/OpcUAConnector.java
@@ -5,6 +5,7 @@ import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
 import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 
 import java.util.List;
 
@@ -18,13 +19,13 @@ public class OpcUAConnector {
         try {
             org.apache.log4j.BasicConfigurator.configure(new NullAppender());
             List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
-            //System.out.println("Endpoints - " + endpoints);
+            EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), "192.168.0.122", 4840);
 
             /*now we want to read specific nodes from the simulator
             firstly we create a OpcUaClientConfigBuilder object */
             OpcUaClientConfigBuilder configBuilder = new OpcUaClientConfigBuilder();
             // we set the endpoint of our object to the first endpoint from the list
-            configBuilder.setEndpoint(endpoints.get(0));
+            configBuilder.setEndpoint(configPoint);
 
             /*now we want to create a OpcUaClient object with the configuration from the previously created
             config object*/
